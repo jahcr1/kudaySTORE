10affb146a0e502f2e16663d590de2673d7d4491
jahcr1 <martineta_contreras@hotmail.com>
2024-11-06 04:47:06 -0300

    Agregando paginas.php a /vistas

--- vistas/cartucheras.php
+++ vistas/cartucheras.php
@@ -1,4 +1,6 @@
-
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="es">
 
@@ -7,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>TIENDA Kuday</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="CSS/styles.css">
+    <link rel="stylesheet" href="../CSS/styles.css">
 
 </head>
 
@@ -27,19 +29,19 @@
                 <div class="collapse navbar-collapse" id="navbarScroll">
                     <ul class="navbar-nav ms-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 600px; margin-right:50px;">
                         <li class="nav-item">
-                            <a class="nav-link active boton-nav" href="index.php#inicio">Inicio</a>
+                            <a class="nav-link active boton-nav" href="../index.php#inicio">Inicio</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link active boton-nav" aria-current="page" href="#promociones">Promociones</a>
+                            <a class="nav-link active boton-nav" aria-current="page" href="promociones.php">Promociones</a>
                         </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle active boton-nav" href="tienda.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Productos
                             </a>
                             <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="cartucheras.php">Cartucheras / Neceser</a></li>
+                                <li><a class="dropdown-item" href="neceser.php">Neceser</a></li>
                                 <hr class="dropdown-divider">
-                                <li><a class="dropdown-item" href="setsmateros.php">Sets Materos</a></li>
+                                <li><a class="dropdown-item" href="setmatero.php">Sets Materos</a></li>
                                 <hr class="dropdown-divider">
                                 <li><a class="dropdown-item " href="billeteras.php">Billeteras</a></li>
                                 <hr class="dropdown-divider">
@@ -61,12 +63,12 @@
     </header>
 
     <section id="slide-cartuchera">
-        
+
         <div class="container">
 
-        <div class="container-fluid slide-tienda">
-            
-            <div id="carouselExampleAutoplaying" style="margin-top: 100px;" class="carousel slide" data-bs-ride="carousel">
+            <div class="container-fluid slide-tienda">
+
+                <div id="carouselExampleAutoplaying" style="margin-top: 100px;" class="carousel slide" data-bs-ride="carousel">
                     <div class="carousel-inner contenedor-carusel1 border rounded">
                         <div class="carousel-item active">
                             <div class="container mx-auto">
@@ -110,10 +112,10 @@
                         <span class="carousel-control-next-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">Next</span>
                     </button>
-    
+
                 </div>
-            
-        </div>
+
+            </div>
 
     </section>
 
@@ -126,35 +128,46 @@
 
         <div class="container text-center" style="margin-top: 50px;">
             <div class=" row m-1">
-                <?php 
-                include('componentes/conexion.php');
-                $consultar_productos = mysqli_query($conexion, "SELECT * FROM productos WHERE categoria_producto = 'Cartuchera'");
+                
+                <!-- ACA LISTAMOS SOLO LAS CARTUCHERAS DESDE LA BD -->
+                <?php
+                include('../componentes/conexion.php');
+                $consultar_productos = mysqli_query($conexion, "SELECT * FROM productos WHERE categoria = 'Cartuchera'");
                 while ($listar_productos = mysqli_fetch_assoc($consultar_productos)) {
                 ?>
-                             
-                <div class="col-md-3 m-2 fondo-card-tienda align-content-center">
-                    <img src="componentes/imagen_producto.php?id=<?php echo $listar_productos['id_producto']; ?>" style="padding: 10px" class="img-promo-tienda img-fluid img3">
-                </div>
-                <div class="card col-md-7 m-2">
-                    <div class="card-body card-body-tienda ">
-                        <h5 class="card-title text-center p-2 item-card "><?php echo $listar_productos['nombre_producto']; ?></h5>
-                        <p class="text-start p-2"><span style="font-weight: bold;">Articulo: </span><?php echo $listar_productos['categoria_producto']; ?></p>
-                        <p class="text-start p-2" style="border-top: 1px solid black;"><span style="font-weight: bold;">Precio: </span><?php echo $listar_productos['precio_producto']; ?></p>
-                        <p class="text-start p-2" style="border-top: 1px solid black;"><span style="font-weight: bold;">Descripción: </span><?php echo $listar_productos['descripcion_producto']; ?></p>
-                        <a href="./index.php" class="btn btn-danger w-100 mt-auto">Agregar al carrito</a>
+                    <div class="listas">
+                        <div class="col-md-3 m-2 contenedor-foto align-self-center">
+                            <?php if (!empty($listar_productos['ci_imagen_producto'])): ?>
+                                <?php
+                                $img_data = base64_encode($listar_productos['ci_imagen_producto']);
+                                $img_type = $listar_productos['formato_imagen'];
+                                echo "<img src='data:$img_type;base64,$img_data' class='imagen-render' alt='Imagen del producto'>";
+                                ?>
+                            <?php else: ?>
+                                <p>Sin imagen disponible</p>
+                            <?php endif; ?>
+                        </div>
+                        <div class="card contenedor-detalle m-2">
+                            <div class="card-body card-body-tienda ">
+                                <h5 class="card-title text-center p-2 item-card "><?php echo $listar_productos['nombre']; ?></h5>
+                                <p class="text-start p-2"><span style="font-weight: bold;">Articulo: </span><span class="dato"><?php echo $listar_productos['categoria']; ?></span></p>
+                                <p class="text-start p-2" style="border-top: 1px solid black;"><span style="font-weight: bold;">Precio: </span><span class="dato">$<?php echo $listar_productos['precio']; ?></span></p>
+                                <p class="text-start p-2" style="border-top: 1px solid black;"><span style="font-weight: bold;">Descripción: </span><span class="dato"><?php echo $listar_productos['descripcion']; ?></span></p>
+                                <a href="./index.php" class="btn btn-danger add-carrito">Agregar al carrito</a>
+                            </div>
+                        </div>
                     </div>
-                </div>
                 <?php  } ?>
-            
+
             </div>
         </div>
 
     </section>
 
-    
+
 
     <footer>
-        <?php include('foot.php');?>
+        <?php include('foot.php'); ?>
     </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
