a4fc0039810eb40c4a8fe5f43aa053e861e3bf7a
jahcr1 <martineta_contreras@hotmail.com>
2024-11-13 00:36:34 -0300

    Desarrollo en cartucheras.php, banner y fondos mas css

--- vistas/cartucheras.php
+++ vistas/cartucheras.php
@@ -16,54 +16,85 @@ session_start();
 <body class="cartuchera-body">
 
     <header>
-     <nav class="navbar navbar-expand-lg fixed-top cartuchera-nav">
+        <nav class="navbar navbar-expand-lg fixed-top cartuchera-nav">
             <div class="container-fluid">
+                    <a class="navbar-brand marca align-self-center text-center" href="index.php#inicio">Kuday Artesanias</a>
 
-                <a class="navbar-brand marca align-self-center text-center" href="index.php#inicio">Kuday Artesanias</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
 
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarScroll">
-                    <ul class="navbar-nav ms-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 600px; margin-right:50px;">
-                        <li class="nav-item">
-                            <a class="nav-link active boton-nav" href="../index.php#inicio">Inicio</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link active boton-nav" aria-current="page" href="promociones.php">Promociones</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle active boton-nav" href="tienda.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Productos
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="neceser.php">Neceser</a></li>
-                                <hr class="dropdown-divider">
-                                <li><a class="dropdown-item" href="setmatero.php">Sets Materos</a></li>
-                                <hr class="dropdown-divider">
-                                <li><a class="dropdown-item " href="billeteras.php">Billeteras</a></li>
-                                <hr class="dropdown-divider">
-                                <li><a class="dropdown-item" href="bolsomatero.php">Bolso Matero</a></li>
-                                <hr class="dropdown-divider">
-                                <li><a class="dropdown-item" href="bandoleras.php">Bandoleras</a></li>
-                                <hr class="dropdown-divider">
-                                <li><a class="dropdown-item" href="varios.php">Varios</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link active boton-nav" href="#">Contactános</a>
-                        </li>
-                    </ul>
-                </div>
+                    <div class="collapse navbar-collapse" id="navbarScroll">
+                        <ul class="navbar-nav ms-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 600px; margin-right:50px;">
+                            <li class="nav-item">
+                                <a class="nav-link active boton-nav" href="../index.php#inicio">Inicio</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active boton-nav" aria-current="page" href="promociones.php">Promociones</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle active boton-nav" href="tienda.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Productos
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="neceser.php">Neceser</a></li>
+                                    <hr class="dropdown-divider">
+                                    <li><a class="dropdown-item" href="setmatero.php">Sets Materos</a></li>
+                                    <hr class="dropdown-divider">
+                                    <li><a class="dropdown-item " href="billeteras.php">Billeteras</a></li>
+                                    <hr class="dropdown-divider">
+                                    <li><a class="dropdown-item" href="bolsomatero.php">Bolso Matero</a></li>
+                                    <hr class="dropdown-divider">
+                                    <li><a class="dropdown-item" href="bandoleras.php">Bandoleras</a></li>
+                                    <hr class="dropdown-divider">
+                                    <li><a class="dropdown-item" href="varios.php">Varios</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active boton-nav" href="#">Contactános</a>
+                            </li>
+                       </ul>
+                 </div>
             </div>
         </nav>
-
     </header>
 
     <section id="slide-cartuchera">
 
         <div class="container contenedor-slide-cartuchera">
+            <div class="box1">
+                <div class="box1-index">
+                    <p class="propaganda1">En Kuday vas a encontrar promos, articulos unicos y más...</p>
+                </div>
+                <a id="cartuchera-bounce" class="propaganda2" href="../vistas/promociones.php" target="_blank">Ver Promociones..</a>
+            </div>
+            <div class="box2">
+                <div class="imagen-logo">
+                    <img src="../images/logo/logo.png" alt="">
+                </div>
+                <div style="display: none;">
+                    <svg viewBox="0 0 250 80" xmlns="http://www.w3.org/2000/svg">
+                        <style>
+                        .small {
+                            font: italic 13px sans-serif;
+                         }
+                        .heavy {
+                            font: bold 25px sans-serif;
+                          }
+
+                        .Rrrrr {
+                            font: italic 32px serif;
+                            fill: red;
+                         }
+                        </style>
+
+                        <text x="20" y="35" class="small">En</text>
+                        <text x="40" y="35" class="heavy">Kuday</text>
+                        <text x="25" y="55" class="small">festejamos</text>
+                        <text x="85" y="65" class="Rrrrr">El Verano!</text>
+                    </svg>
+                </div>
+            </div>
             
         </div>
 
@@ -73,7 +104,7 @@ session_start();
     <section id="cartucheras" style="margin-top: 20px; padding: 20px 0 20px 0;">
 
         <div class="container">
-            <h3 class="text-center titulo" style="margin-top: 80px;">Cartucheras</h3>
+            <h3 class="text-center titulo-categoria" style="margin-top: 80px;">Cartucheras</h3>
         </div>
 
         <div class="container text-center" style="margin-top: 50px;">
@@ -121,6 +152,28 @@ session_start();
     </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <!-- Incluyendo GSAP desde un CDN -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.2/gsap.min.js"></script>
+    <script>
+        gsap.to("#cartuchera-bounce", {
+            duration: 1.5,
+            y: 50,
+            ease: "bounce.out", // Efecto de rebote realista
+            repeat: -1, // Animación infinita
+            yoyo: true // Vuelve a su posición original para continuar rebotando
+        });
+    </script>
+
+    <script>
+        let words = gsap.utils.toArray("svg text"),
+        tl = gsap.timeline({delay: 0.5}),
+        timePerCharacter = 0.2;
+
+        words.forEach(el => {
+            tl.from(el, {text: "", duration: el.innerHTML.length * timePerCharacter, ease: "none"});
+        });
+    </script>
+
 </body>
 
 </html>
