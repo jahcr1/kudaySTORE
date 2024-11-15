d91d12e52a3d06979f6bafcf70d3148bacb154cf
jahcr1 <martineta_contreras@hotmail.com>
2024-11-15 03:16:43 -0300

    modificando vistas principal cartuchera, tipografias y listado grid en vistas

--- index.php
+++ index.php
@@ -4,7 +4,12 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Kuday Argentina</title>
+  <title>Kuday Argentina Sitio Oficial</title>
+
+  <!-- FAMILIAS TIPOGRAFICAS DE GOOGLE FONTS -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Aclonica&family=Bangers&family=Barrio&family=Chango&family=Chewy&family=Chicle&family=Delius+Unicase:wght@400;700&family=Flavors&family=Gwendolyn:wght@400;700&family=Ingrid+Darling&family=Just+Me+Again+Down+Here&family=Kablammo&family=Lumanosimo&family=Martian+Mono:wght@100..800&family=Mystery+Quest&family=Pacifico&family=Rubik+Puddles&family=Shrikhand&family=Sour+Gummy:ital,wght@0,100..900;1,100..900&family=Unkempt:wght@400;700&family=Vibur&family=Yomogi&display=swap" rel="stylesheet">
 
   <!-- ICONOS DE BOOTSTRAP -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
@@ -13,17 +18,17 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
   <!-- CSS PROPIO-->
-  <link rel="stylesheet" href="CSS/styles.css">
+  <link rel="stylesheet" href="./CSS/styles.css">
 
 </head>
 
-<body>
+<body class="index-body">
 
   <header>
 
-    <nav class="navbar navbar-expand-lg barranav fixed-top border rounded">
+    <nav class="navbar navbar-expand-lg index-nav fixed-top">
       <div class="container-fluid">
-        <a class="navbar-brand marca align-self-center text-center ms-auto" href="index.php#inicio">Kuday Artesanias</a>
+        <a class="navbar-brand marca align-self-center text-center" href="index.php#inicio" >Kuday Artesanias</a>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
--- vistas/cartucheras.php
+++ vistas/cartucheras.php
@@ -7,8 +7,20 @@ session_start();
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>TIENDA Kuday</title>
+    <title>Cartucheras Kuday | Sitio Oficial</title>
+    
+    <!-- FAMILIAS TIPOGRAFICAS DE GOOGLE FONTS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Aclonica&family=Bangers&family=Barrio&family=Chango&family=Chewy&family=Chicle&family=Delius+Unicase:wght@400;700&family=Flavors&family=Gwendolyn:wght@400;700&family=Ingrid+Darling&family=Just+Me+Again+Down+Here&family=Kablammo&family=Lumanosimo&family=Martian+Mono:wght@100..800&family=Mystery+Quest&family=Pacifico&family=Rubik+Puddles&family=Shrikhand&family=Sour+Gummy:ital,wght@0,100..900;1,100..900&family=Unkempt:wght@400;700&family=Vibur&family=Yomogi&display=swap" rel="stylesheet">
+    
+    <!-- ICONOS DE BOOTSTRAP -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+    <!-- CSS DE BOOTSTRAP -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    
+    <!-- CSS PROPIO -->
     <link rel="stylesheet" href="../CSS/styles.css">
 
 </head>
@@ -18,7 +30,7 @@ session_start();
     <header>
         <nav class="navbar navbar-expand-lg fixed-top cartuchera-nav">
             <div class="container-fluid">
-                    <a class="navbar-brand marca align-self-center text-center" href="index.php#inicio">Kuday Artesanias</a>
+                    <a class="navbar-brand marca align-self-center text-center" href="../index.php#inicio">Kuday Artesanias</a>
 
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
@@ -101,45 +113,43 @@ session_start();
     </section>
 
 
-    <section id="cartucheras" style="margin-top: 20px; padding: 20px 0 20px 0;">
+    <section id="cartucheras" style="margin-top:20px; padding:20px 0 20px 0;">
 
         <div class="container">
             <h3 class="text-center titulo-categoria" style="margin-top: 80px;">Cartucheras</h3>
         </div>
 
-        <div class="container text-center" style="margin-top: 50px;">
-            <div class=" row m-1">
-                
+        <div style="margin-top: 50px;">
+            <div class="grid-container">
                 <!-- ACA LISTAMOS SOLO LAS CARTUCHERAS DESDE LA BD -->
                 <?php
                 include('../componentes/conexion.php');
                 $consultar_productos = mysqli_query($conexion, "SELECT p.*, c.nombre AS categoria_nombre FROM productos p JOIN categorias c ON p.categoria_id = c.id WHERE p.categoria_id = '1'");
                 while ($listar_productos = mysqli_fetch_assoc($consultar_productos)) {
                 ?>
-                    <div class="listas">
-                        <div class="col-md-3 m-2 contenedor-foto align-self-center">
+                    <div class="producto">
+                        <div class="contenedor-foto">
                             <?php if (!empty($listar_productos['ci_imagen_producto'])): ?>
                                 <?php
                                 $img_data = base64_encode($listar_productos['ci_imagen_producto']);
                                 $img_type = $listar_productos['formato_imagen'];
-                                echo "<img src='data:$img_type;base64,$img_data' class='imagen-render-vistas' alt='Imagen del producto'>";
+                                echo "<img src='data:$img_type;base64,$img_data' class='imagen-producto' alt='Imagen del producto'>";
                                 ?>
                             <?php else: ?>
                                 <p>Sin imagen disponible</p>
                             <?php endif; ?>
                         </div>
-                        <div class="card contenedor-detalle m-2">
-                            <div class="card-body card-body-tienda ">
-                                <h5 class="card-title text-center p-2 item-card "><?php echo $listar_productos['nombre']; ?></h5>
-                                <p class="text-start p-2"><span style="font-weight: bold;">Articulo: </span><span class="dato"><?php echo $listar_productos['categoria_nombre']; ?></span></p>
-                                <p class="text-start p-2" style="border-top: 1px solid black;"><span style="font-weight: bold;">Precio: </span><span class="dato">$<?php echo $listar_productos['precio']; ?></span></p>
-                                <p class="text-start p-2" style="border-top: 1px solid black;"><span style="font-weight: bold;">Descripción: </span><span class="dato"><?php echo $listar_productos['descripcion']; ?></span></p>
+                        <div class="card contenedor-detalle">
+                            <div class="card-body">
+                                <h5 class="card-title p-2 item-card"><?php echo $listar_productos['nombre']; ?></h5>
+                                <p class="text-start p-3"><strong>Articulo : </strong><span class="dato"><?php echo $listar_productos['categoria_nombre']; ?></span></p>
+                                <p class="text-start p-3" style="border-top: 1px solid black;"><strong>Precio : </strong><span class="dato">$<?php echo $listar_productos['precio']; ?></span></p>
+                                <p class="text-start p-3" style="border-top: 1px solid black;"><strong>Descripción : </strong><span class="dato"><?php echo $listar_productos['descripcion']; ?></span></p>
                                 <a href="./index.php" class="btn btn-danger add-carrito">Agregar al carrito</a>
                             </div>
                         </div>
                     </div>
-                <?php  } ?>
-
+                <?php } ?>
             </div>
         </div>
 
@@ -147,11 +157,49 @@ session_start();
 
 
 
-    <footer>
-        <?php include('../foot.php'); ?>
+    <footer class="cartuchera-footer">
+        <main class="container-fluid">
+            <div class="row contenedor_footer-cartuchera">  
+                <div class="col-xl-3 col-md-3 col-sm-10 p-2">
+                    <h6 class="h6-footer-cartuchera"><i class="bi bi-tencent-qq"></i>  CONTÁCTANOS</h6>
+                    <p class="p-footer-cartuchera"><i class="bi bi-telephone-fill"></i>   +54 9 0297 432-1429</p>
+                    <p class="p-footer-cartuchera"><i class="bi bi-whatsapp"></i>   +54 9 0297 432-1429</p>
+                    <p class="p-footer-cartuchera"><i class="bi bi-geo-alt-fill"></i>   Gral. Araóz de Lamadrid 425</p>
+                </div>
+                <div class="col-xl-3 col-md-3 col-sm-6 p-2">
+                    <h6 class="h6-footer-cartuchera"><i class="bi bi-tencent-qq"></i>  MEDIOS DE PAGO</h6>
+                    <div class="contenedor-cards p-2">
+                        <img src="../images/cards/visa.png"  alt="mp">
+                        <img src="../images/cards/naranja.png" alt="visa">
+                        <img src="../images/cards/mercadopago2.png" alt="visa">
+                        <img src="../images/cards/pagofacil.png" alt="visa">
+                        <img src="../images/cards/efectivo.png" alt="visa">
+                        <img src="../images/cards/rapipago.png" alt="">
+                    </div>
+                </div>
+                <div class="col-xl-3 col-md-3 col-sm-6 p-2">
+                    <h6 class="h6-footer-cartuchera"><i class="bi bi-tencent-qq"></i>  FORMAS DE ENVIO</h6>
+                    <div class="contenedor-cards p-2">
+                        <img src="../images/cards/andreani.png" alt="">
+                        <img src="../images/cards/correoarg.png" alt="">
+                    </div>
+                </div>
+                <div class="row d-flex">
+                    <h4>Seguinos en nuestras redes!</h4>
+                    <div class="redes-icons-footer-cartuchera ">
+                        <a href="https://www.facebook.com/dai.quiroga.7" target="_"><i class="bi bi-facebook"></i></a>
+                        <a href="https://www.facebook.com/dai.quiroga.7"><i class="bi bi-instagram"></i></a>
+                        <a href="https://www.facebook.com/dai.quiroga.7"><i class="bi bi-pinterest"></i></a>
+                    </div>
+                </div>
+            </div>
+        </main>
+
+        <p style="font-size:10px;background-color:white; color:black;width:100%;padding:0 5px;"><i class="bi bi-c-circle"></i> COPYRIGHT KUDAY ARTESANIAS & DEVCR1 2021. TODOS LOS DERECHOS RESERVADOS.</p>
     </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    
     <!-- Incluyendo GSAP desde un CDN -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.2/gsap.min.js"></script>
     <script>
