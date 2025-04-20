f245bc049e0dd07f260a94f21823638e641bbed6
jahcr1 <martineta_contreras@hotmail.com>
2025-04-20 10:32:52 -0300

    UPD en costos de envios por provincia

--- componentes/calcular_envio.php
+++ componentes/calcular_envio.php
@@ -4,33 +4,33 @@ if (isset($_POST['provincia'])) {
 
     // Simulación de costos de envío según provincia
     $costosEnvio = [
-        "Buenos Aires" => 500,   // Buenos Aires
-        "Cordoba" => 700,   // Córdoba
-        "Santa Cruz" => 900,   // Santa Cruz
-        "Catamarca" => 900,   // Catamarca
-        "Chaco" => 900,   // Chaco
-        "Chubut" => 900,   // Chubut
-        "Corrientes" => 900,   // Corrientes
-        "Entre Rios" => 900,   // Entre Rios
-        "Formosa" => 900,   // Formosa
-        "Jujuy" => 900,   // Jujuy
-        "La Pampa" => 900,   // La Pampa
-        "La Rioja" => 900,   // La Rioja
-        "Mendoza" => 900,   // Mendoza
-        "Misiones" => 900,   // Misiones
-        "Neuquen" => 900,   // Neuquén
-        "Rio Negro" => 900,   // Rio Negro
-        "Salta" => 900,   // Salta
-        "San Juan" => 900,   // San Juan
-        "San Luis" => 900,   // San Luis
-        "Santa Fe" => 900,   // Santa Fe
-        "Santiago Del Estero" => 900,   // Santiago Del Estero
-        "Tierra Del Fuego" => 900,   // Tierra Del Fuego
-        "Tucuman" => 900,   // Tucumán
+        "Buenos Aires" => 8500,   // Buenos Aires
+        "Cordoba" => 3500,   // Córdoba
+        "Santa Cruz" => 10500,   // Santa Cruz
+        "Catamarca" => 6900,   // Catamarca
+        "Chaco" => 10500,   // Chaco
+        "Chubut" => 9500,   // Chubut
+        "Corrientes" => 6500,   // Corrientes
+        "Entre Rios" => 6500,   // Entre Rios
+        "Formosa" => 7500,   // Formosa
+        "Jujuy" => 6500,   // Jujuy
+        "La Pampa" => 6500,   // La Pampa
+        "La Rioja" => 5500,   // La Rioja
+        "Mendoza" => 6500,   // Mendoza
+        "Misiones" => 6500,   // Misiones
+        "Neuquen" => 8500,   // Neuquén
+        "Rio Negro" => 8500,   // Rio Negro
+        "Salta" => 7500,   // Salta
+        "San Juan" => 6500,   // San Juan
+        "San Luis" => 6500,   // San Luis
+        "Santa Fe" => 6500,   // Santa Fe
+        "Santiago Del Estero" => 9500,   // Santiago Del Estero
+        "Tierra Del Fuego" => 11000,   // Tierra Del Fuego
+        "Tucuman" => 7500,   // Tucumán
         // Agregar más provincias con sus respectivos costos
     ];
 
-    $costoEnvio = isset($costosEnvio[$provincia]) ? $costosEnvio[$provincia] : 1000;
+    $costoEnvio = isset($costosEnvio[$provincia]) ? $costosEnvio[$provincia] : 10000;
 
     echo json_encode(["costoEnvio" => $costoEnvio]);
 }
