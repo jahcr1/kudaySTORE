387c41e8b2b2d0c01182f8807fd9d9c4903f3cbd
jahcr1 <martineta_contreras@hotmail.com>
2025-07-08 17:23:44 -0300

    Agregando rama de desarrollo ui_gallery y agregando link de lib lightgallery

--- producto.php
+++ producto.php
@@ -53,6 +53,9 @@ if (!$producto) {
     <!-- ICONOS DE FONTAWESOME -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" rel="stylesheet">
 
+    <!-- CSS LIGHTGALLERY  -->
+     <link type="text/css" rel="stylesheet" href="css/lightgallery-bundle.css" />
+
     <!-- CSS DE BOOTSTRAP -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     
